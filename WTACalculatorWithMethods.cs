d997fcc43a53958ac8bb1597f385498f5f627a69
Maria Vasquez <87947324+mariavasqueze@users.noreply.github.com>
2022-02-11 01:08:36 -0500

    Update WTACalculatorWithMethods.cs
    Final Exercise version. Working properly with methods

--- WTACalculatorWithMethods.cs
+++ WTACalculatorWithMethods.cs
@@ -1,5 +1,4 @@
 // The idea is to make the Weigthed Average Calculator from the other file but with methods.
-// Missing changes
 
 using System;
 using static System.Console;
@@ -17,30 +16,29 @@ namespace WeightedAverageCalc
             const float FINAL_EXAM_PERCENTAGE = 0.3f;
 
             // variables for storing the grades of a student
-            float assignments = 80f;
-            float midtermExam = 90f;
-            float quiz1 = 80f;
-            float quiz2 = 80f;
-            float finalExam = 80f;
+            float assignments;
+            float midtermExam;
+            float quiz1;
+            float quiz2;
+            float finalExam;
 
             float weightedExams = 0;
             float totalWeightedAverage = 0;
 
-
             Title = "CSIS1175 - Assignment 3 - By Maria Vasquez";
 
-            DisplayBanner("Total Weighted Average Calculator");
+            DisplayBanner("Total Weighted Average Calculator\n");
 
-            
+
             // The user enters the grades, the program gets them and stores them in the corresponding variable
             assignments = GetUserInput("Assignments");
-            midtermExam= GetUserInput("Midterm Exam");
+            midtermExam = GetUserInput("Midterm Exam");
             quiz1 = GetUserInput("Quiz1");
             quiz2 = GetUserInput("Quiz2");
             finalExam = GetUserInput("Final Exam");
 
-            
-            // Total Weighted Avergae is sum of products of grades and their weight
+
+            // Total Weighted Average is sum of products of grades and their weight
             totalWeightedAverage += WeightedGrade(assignments, ASSIGNMENTS_PERCENTAGE);
             totalWeightedAverage += WeightedGrade(midtermExam, MIDTERM_EXAM_PERCENTAGE);
             totalWeightedAverage += WeightedGrade((quiz1 + quiz2), QUIZ_PERCENTAGE);
@@ -58,7 +56,7 @@ namespace WeightedAverageCalc
             WriteLine("----------------------------------------");
 
             // change the following line of code such that the Floor value of totalWeightedAverage is displayed on Console
-            DisplayTableRow("Weighted Total", 1, totalWeightedAverage); //** Change only this line  
+            DisplayTableRow("Weighted Total", 1, (float)Math.Floor(totalWeightedAverage));  
 
             WriteLine("\n");
 
@@ -68,17 +66,16 @@ namespace WeightedAverageCalc
             weightedExams /= 0.8f;
 
             // change the following line of code such that the Ceiling value of weightedExams is displayed on Console
-            WriteLine("The Weighted Average Total on Exams (Midterm, Quizzes, Final exam) is {0:F2}", weightedExams); //** Change only this line 
+            WriteLine("The Weighted Average Total on Exams (Midterm, Quizzes, Final exam) is {0:F2}", Math.Ceiling(weightedExams)); 
             WriteLine("If WAT-on-Exams is less than 50, the student fails the course.");
-            
+
             ReadKey();
-        }  
-        
+        }
+
         //  your method definitions are written here (out of Main method). A method cannot be defined inside another method
-        static string DisplayBanner(string banner)
+        static void DisplayBanner(string banner)
         {
             WriteLine(banner);
-            return banner;
         }
 
         static float GetUserInput(string gradeName)
@@ -86,37 +83,30 @@ namespace WeightedAverageCalc
             string userInput;
             float studentGrade;
 
-            WriteLine($"Enter grade for {gradeName}: ");
+            WriteLine($"Enter grade for {gradeName}");
             userInput = ReadLine();
             studentGrade = float.Parse(userInput);
-
-            return studentGrade;   
+            return studentGrade;
         }
 
-        static string DisplayTableRow(string name, string percentage, string grade)
+        static void DisplayTableRow(string name, string percentage, string grade)
         {
-            string printLine;
-
-            WriteLine($"{name}\n, {percentage}\n, {grade}\n");
-
-            return printLine;
+            WriteLine("{0,14}{1,13:P0} {2,-15}", name, percentage, grade);
         }
 
-        static string DisplayTableRow(string name, float percentage, float grade)
+        static void DisplayTableRow(string name, float percentage, float grade)
         {
-            string printLine;
-
-            WriteLine($"{name}\n, {percentage}\n, {grade}\n"); //WriteLine("{0} {1} {2}", name, percentage, grade);
-
-            return printLine;
+            WriteLine("{0,14}{1,13:P0} {2,-15}", name, percentage, grade);
         }
 
         static float WeightedGrade(float grade, float weight)
         {
             float weightCalc = grade * weight;
-
             return weightCalc;
         }
+
     }
 
 }
+
+
